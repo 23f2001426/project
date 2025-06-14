cecb02ce3d4df3a0c71c41071712447b51cbdb4b
DerrickUnleashed <derricksamuel.r@gmail.com>
2025-06-15 01:50:14 +0530

    Final

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("knowledge_base.db")
+conn = sqlite3.connect("knowledge_base_helper.db")
 cur = conn.cursor()
 
 cur.execute("SELECT COUNT(*) FROM discourse_chunks")
